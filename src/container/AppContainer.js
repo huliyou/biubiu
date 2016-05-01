a77a49960f0b3b0263374a66991ff82e760b84e6
leiyouwho <liyoupie@gmail.com>
2016-05-02 00:51:58 +0800

    修复Tab点击切换

--- src/container/AppContainer.js
+++ src/container/AppContainer.js
@@ -20,38 +20,96 @@ const HomeIndex = 0;
 const MessageIndex = 1;
 const MeIndex = 2;
 
-const AppContainer = (props) => {
-  const _clickTabAtIndex = (index: number) => {
-    switch (index) {
-      case HomeIndex:
-        return () => {
-          // props.dispatch(AppAction.selectHomePage());
-          props.dispatch(routeActions.push('/app/Home'));
-        };
-      case MessageIndex:
-        return () => {
-          // props.dispatch(AppAction.selectMessagePage());
-          props.dispatch(routeActions.push('/app/Message'));
-        };
-      case MeIndex:
-        return () => {
-          // props.dispatch(AppAction.selectMePage());
-          props.dispatch(routeActions.push('/app/Me'));
-        };
-      default:
-        return () => console.log('err');
+class AppContainer extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      value: 'a',
     }
-  };
+  }
+  // const _clickTabAtIndex = (index: number) => {
+  //   switch (index) {
+  //     case HomeIndex:
+  //       return () => {
+  //         // props.dispatch(AppAction.selectHomePage());
+  //         props.dispatch(routeActions.push('/app/Home'));
+  //       };
+  //     case MessageIndex:
+  //       return () => {
+  //         // props.dispatch(AppAction.selectMessagePage());
+  //         props.dispatch(routeActions.push('/app/Message'));
+  //       };
+  //     case MeIndex:
+  //       return () => {
+  //         // props.dispatch(AppAction.selectMePage());
+  //         props.dispatch(routeActions.push('/app/Me'));
+  //       };
+  //     default:
+  //       return () => console.log('err');
+  //   }
+  // };
 
-  return (
-    <View>
-      <Tabs>
-        <Tab label = "沙龙" />
-        <Tab label = "消息" />
-        <Tab label = "我的" />
-      </Tabs>
-    </View>
-  );
+  // handleChange = (value) => {
+  //   console.log('value', value);
+  //   this.setState({
+  //     value: value,
+  //   });
+  // };
+  handleChange(value) {
+    console.log('value', value);
+    this.setState({
+      value: value,
+    });
+  }
+  render() {
+    return(
+      <View>
+        <Tabs
+          value={this.state.value}
+          initialSelectedIndex={2}
+          onChange={(e) => {
+            console.log('click: ', e);
+          }}
+
+        >
+          <Tab
+            label="沙龙"
+            value="a"
+            onClick={() => {
+              this.setState({
+                value: 'a',
+              });
+            }}
+          >
+            <div>hehe</div>
+          </Tab>
+          <Tab
+            label="消息"
+            value="b"
+            onTouchTap ={(e) => {
+              console.log('active: ', e);
+            }}
+            onClick={() => {
+              this.setState({
+                value: 'b',
+              });
+            }}
+          >
+            <div>sdfsd</div>
+          </Tab>
+          <Tab
+            label="我的"
+            value="c"
+            onClick={() => {
+              this.setState({
+                value: 'c',
+              });
+            }}
+          />
+        </Tabs>
+      </View>
+    );
+  }
 };
 
 AppContainer.propTypes = propTyeps;
